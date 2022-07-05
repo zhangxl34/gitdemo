0e846c0515530cb09c1dd1f8968547ab822edc8a
zhangxl35 <zhangxl35qq.com>
2022-07-06 06:50:42 +0800

    first git projecr demo

---
+++ src/main/java/com/spdb/App.java
@@ -0,0 +1,14 @@
+package com.spdb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("see see you");
+        System.out.println( "Hello World!" );
+    }
+}
